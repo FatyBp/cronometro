f5b4b484db343c491a252013c52c4280d743e0a8
Fatybp <fatimabenitez796@gmail.com>
2023-05-30 13:30:34 -0600

    cronometro

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'cronometro';
+  public mensaje : string = '';
+  public actualizar(tiempo:number):void{
+    this.mensaje = tiempo + '(se actualiza cada 10 segundos)'
+  }
+
+}
